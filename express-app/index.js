77363a10ffac5689a2ad7658b49cc3c542e877e3
Sami Reimavuo <sami@reimavuo.net>
2023-06-06 10:38:16 +0300

    Combine GHA jobs to one

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! All along the Watchtower!</h1>')
+  res.send('<h1>Hello World! And Fly.io!</h1>')
 })
 
 const PORT = 8080
